24c301a20e485bbeb2e736cbf5f788c1fe34d086
0victorsales <victorhsales0@gmai.com>
2024-03-20 16:02:47 -0300

    feat: finalizado api's do asana

--- src/asana_files/api_asana.py
+++ src/asana_files/api_asana.py
@@ -10,6 +10,7 @@ configuration = asana.Configuration()
 configuration.access_token = access_token
 api_client = asana.ApiClient(configuration)
 projects_api_instance = asana.ProjectsApi(api_client)
+tasks_api_instance = asana.TasksApi(api_client)
 
 
 #SECTION - Sessão projetos
@@ -61,5 +62,49 @@ def delete_project():
         print("Não foi possivel excluir o projeto, erro: %s\n" % e)
 
 
-a = delete_project()
+#SECTION - Sessão task's
+def get_task():
+    task_gid = "1206889636844883" 
+    opts = {
+        'opt_fields': "actual_time_minutes,approval_status,assignee,assignee.name,assignee_section,assignee_section.name,assignee_status,completed,completed_at,completed_by,completed_by.name,created_at,created_by,custom_fields,custom_fields.asana_created_field,custom_fields.created_by,custom_fields.created_by.name,custom_fields.currency_code,custom_fields.custom_label,custom_fields.custom_label_position,custom_fields.date_value,custom_fields.date_value.date,custom_fields.date_value.date_time,custom_fields.description,custom_fields.display_value,custom_fields.enabled,custom_fields.enum_options,custom_fields.enum_options.color,custom_fields.enum_options.enabled,custom_fields.enum_options.name,custom_fields.enum_value,custom_fields.enum_value.color,custom_fields.enum_value.enabled,custom_fields.enum_value.name,custom_fields.format,custom_fields.has_notifications_enabled,custom_fields.is_formula_field,custom_fields.is_global_to_workspace,custom_fields.is_value_read_only,custom_fields.multi_enum_values,custom_fields.multi_enum_values.color,custom_fields.multi_enum_values.enabled,custom_fields.multi_enum_values.name,custom_fields.name,custom_fields.number_value,custom_fields.people_value,custom_fields.people_value.name,custom_fields.precision,custom_fields.resource_subtype,custom_fields.text_value,custom_fields.type,dependencies,dependents,due_at,due_on,external,external.data,followers,followers.name,hearted,hearts,hearts.user,hearts.user.name,html_notes,is_rendered_as_separator,liked,likes,likes.user,likes.user.name,memberships,memberships.project,memberships.project.name,memberships.section,memberships.section.name,modified_at,name,notes,num_hearts,num_likes,num_subtasks,parent,parent.created_by,parent.name,parent.resource_subtype,permalink_url,projects,projects.name,resource_subtype,start_at,start_on,tags,tags.name,workspace,workspace.name", # list[str] | This endpoint returns a compact resource, which excludes some properties by default. To include those optional properties, set this query parameter to a comma-separated list of the properties you wish to include.
+    }
+
+    try:
+        
+        response = tasks_api_instance.get_task(task_gid, opts)
+        return response
+    except ApiException as e:
+        print("Não foi possivel obter task: %s\n" % e)
+
+def criar_task():
+    project_id = "1206889635666466"
+    body = {"data": {
+        "name": "task", 
+        "approval_status": "approved",
+        "completed": False,
+        "notes": "teste de notas da task",
+        "start_at": "2024-03-20T00:00:00Z", 
+        "due_at": "2024-03-21T00:00:00Z", 
+        "workspace": "1206889103473198",
+        "projects": [project_id]
+        }}
+    opts = {
+        'opt_fields': "actual_time_minutes,approval_status,assignee,assignee.name,assignee_section,assignee_section.name,assignee_status,completed,completed_at,completed_by,completed_by.name,created_at,created_by,custom_fields,custom_fields.asana_created_field,custom_fields.created_by,custom_fields.created_by.name,custom_fields.currency_code,custom_fields.custom_label,custom_fields.custom_label_position,custom_fields.date_value,custom_fields.date_value.date,custom_fields.date_value.date_time,custom_fields.description,custom_fields.display_value,custom_fields.enabled,custom_fields.enum_options,custom_fields.enum_options.color,custom_fields.enum_options.enabled,custom_fields.enum_options.name,custom_fields.enum_value,custom_fields.enum_value.color,custom_fields.enum_value.enabled,custom_fields.enum_value.name,custom_fields.format,custom_fields.has_notifications_enabled,custom_fields.is_formula_field,custom_fields.is_global_to_workspace,custom_fields.is_value_read_only,custom_fields.multi_enum_values,custom_fields.multi_enum_values.color,custom_fields.multi_enum_values.enabled,custom_fields.multi_enum_values.name,custom_fields.name,custom_fields.number_value,custom_fields.people_value,custom_fields.people_value.name,custom_fields.precision,custom_fields.resource_subtype,custom_fields.text_value,custom_fields.type,dependencies,dependents,due_at,due_on,external,external.data,followers,followers.name,hearted,hearts,hearts.user,hearts.user.name,html_notes,is_rendered_as_separator,liked,likes,likes.user,likes.user.name,memberships,memberships.project,memberships.project.name,memberships.section,memberships.section.name,modified_at,name,notes,num_hearts,num_likes,num_subtasks,parent,parent.created_by,parent.name,parent.resource_subtype,permalink_url,projects,projects.name,resource_subtype,start_at,start_on,tags,tags.name,workspace,workspace.name", # list[str] | This endpoint returns a compact resource, which excludes some properties by default. To include those optional properties, set this query parameter to a comma-separated list of the properties you wish to include.
+    }
+
+    try:
+        response = tasks_api_instance.create_task(body, opts)
+        return response
+    except ApiException as e:
+        print("Erro ao criar task: %s\n" % e)
+
+def delete_task():
+    task_gid = "" 
+    try:
+        response = tasks_api_instance.delete_task(task_gid)
+        return response
+    except ApiException as e:
+        print("Exception when calling TasksApi->delete_task: %s\n" % e)
+
+a = get_task()
 print(a)
--- t.py
+++ t.py
@@ -1,6 +1,7 @@
 import asana
 from asana.rest import ApiException
 from pprint import pprint
+
 from dotenv import load_dotenv
 import os
 import json
@@ -12,13 +13,13 @@ configuration.access_token = access_token
 api_client = asana.ApiClient(configuration)
 
 # create an instance of the API class
-projects_api_instance = asana.ProjectsApi(api_client)
-project_gid = "1206890793067999" # str | Globally unique identifier for the project.
+tasks_api_instance = asana.TasksApi(api_client)
+task_gid = "1206891335169286" 
 
 
 try:
-    # Delete a project
-    api_response = projects_api_instance.delete_project(project_gid)
+    # Delete a task
+    api_response = tasks_api_instance.delete_task(task_gid)
     pprint(api_response)
 except ApiException as e:
-    print("Exception when calling ProjectsApi->delete_project: %s\n" % e)
+    print("Exception when calling TasksApi->delete_task: %s\n" % e)
